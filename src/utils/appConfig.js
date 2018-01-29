73773676b9235d11c526c4714bf8bd8b013c450e
yang24201 <yang24201@126.com>
2018-01-29 16:23:55 +0800

    add socket support

---
+++ src/utils/appConfig.js
@@ -0,0 +1,17 @@
+/**
+ * Created by yangyang on 2018/1/29.
+ */
+
+// Dev
+export const APP_ID = 'dHMtEcCm3jlfpRwRBzFvTATg-gzGzoHsz'
+export const APP_KEY = '1gKH7vubIldeiHhvQXqpAuaK'
+export const WSS_DOMAIN = 'wss://xyfb-dev.leanapp.cn/'
+
+
+// PROD
+// export const APP_ID = '2R6ElxahrIm3O4xjJF0fpNXO-gzGzoHsz'
+// export const APP_KEY = 'R0kWvaQsdIU6HDJPqxfwkWTw'
+// export const WSS_DOMAIN = 'wss://xyfb.leanapp.cn/'
+
+// socket消息
+export const REQUEST_DRAW_LOTTERY = 1
