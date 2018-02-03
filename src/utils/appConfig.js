558e8c4d2fc6c957f2169327ea4b8323fc212f8f
yang24201 <yang24201@126.com>
2018-02-03 12:18:34 +0800

    add payment control

--- src/utils/appConfig.js
+++ src/utils/appConfig.js
@@ -3,15 +3,15 @@
  */
 
 // Dev
-export const APP_ID = 'dHMtEcCm3jlfpRwRBzFvTATg-gzGzoHsz'
-export const APP_KEY = '1gKH7vubIldeiHhvQXqpAuaK'
-export const WSS_DOMAIN = 'wss://xyfb-dev.leanapp.cn/'
+// export const APP_ID = 'dHMtEcCm3jlfpRwRBzFvTATg-gzGzoHsz'
+// export const APP_KEY = '1gKH7vubIldeiHhvQXqpAuaK'
+// export const WSS_DOMAIN = 'wss://xyfb-dev.leanapp.cn/'
 
 
 // PROD
-// export const APP_ID = '2R6ElxahrIm3O4xjJF0fpNXO-gzGzoHsz'
-// export const APP_KEY = 'R0kWvaQsdIU6HDJPqxfwkWTw'
-// export const WSS_DOMAIN = 'wss://xyfb.leanapp.cn/'
+export const APP_ID = '2R6ElxahrIm3O4xjJF0fpNXO-gzGzoHsz'
+export const APP_KEY = 'R0kWvaQsdIU6HDJPqxfwkWTw'
+export const WSS_DOMAIN = 'wss://xyfb.leanapp.cn/'
 
 // socket消息
 export const REQUEST_DRAW_LOTTERY = 1
