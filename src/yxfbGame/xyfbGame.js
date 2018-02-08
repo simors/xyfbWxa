18fefa1d4f7b442a8d2bae0ae24e747a1283151f
yang24201 <yang24201@126.com>
2018-02-08 10:46:43 +0800

    disable audio when walk

--- src/yxfbGame/xyfbGame.js
+++ src/yxfbGame/xyfbGame.js
@@ -132,9 +132,6 @@ function xyfbGame(opts) {
   
   let innerAudioContext = this.innerAudioContext = wx.createInnerAudioContext()
   innerAudioContext.autoplay = false
-  innerAudioContext.onPlay(() => {
-    // console.log('开始播放')
-  })
   innerAudioContext.onError((res) => {
     console.log(res.errMsg)
     console.log(res.errCode)
@@ -177,7 +174,7 @@ function xyfbGame(opts) {
       let isMove = count % (gameSpeed / config["refreshSpeed"])
       if (isMove == 0) {
         walkedBox.step = walkedBox.step + 1
-        playAudio(gameAudio.walk)
+        // playAudio(gameAudio.walk)
       }
     }
   }
